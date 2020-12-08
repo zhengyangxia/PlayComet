4ba9ade876058f4c042c9bcae3d303bf11ab1b77
Xiaoqiao Xu <xiaoqiao@andrew.cmu.edu>
2020-12-08 10:40:45 -0500

    [hotfix] remove fullscreen for linux

--- main.cpp
+++ main.cpp
@@ -59,7 +59,7 @@ int main(int argc, char **argv) {
 		SDL_WINDOW_OPENGL
 		| SDL_WINDOW_RESIZABLE //uncomment to allow resizing
 		| SDL_WINDOW_ALLOW_HIGHDPI //uncomment for full resolution on high-DPI screens
-		| SDL_WINDOW_FULLSCREEN_DESKTOP
+//		| SDL_WINDOW_FULLSCREEN_DESKTOP
 	);
 
 	//prevent exceedingly tiny windows when resizing:
