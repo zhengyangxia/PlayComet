3a6e5427542ca12a76f452a3b04a404a6107c684
yunfeic <73167381@qq.com>
2020-12-08 02:10:00 -0500

    <feat>:add planets

--- Revolve.cpp
+++ Revolve.cpp
@@ -7,9 +7,15 @@ void Revolve::register_planet(Scene::Transform* t, float period, float dist, glm
 }
 
 Revolve::Revolve(){
-    attr_map.insert(std::pair<std::string, PlanetRevolveAttributes>("Jupiter", {200.f, 3000.f, glm::vec3(0.f, 1.f, 1.f)}));
-    attr_map.insert(std::pair<std::string, PlanetRevolveAttributes>("Earth", {175.f, 2000.f, glm::vec3(0.f, 1.f, 0.f)}));
-    attr_map.insert(std::pair<std::string, PlanetRevolveAttributes>("Mars", {150.f, 2000.f, glm::vec3(0.f, 0.f, 1.f)}));
+    attr_map.insert(std::pair<std::string, PlanetRevolveAttributes>("Jupiter", {200.f, 2000.f, glm::vec3(0.f, 1.f, 1.f)}));
+    attr_map.insert(std::pair<std::string, PlanetRevolveAttributes>("Earth", {175.f, 3000.f, glm::vec3(0.f, 1.f, 0.f)}));
+    attr_map.insert(std::pair<std::string, PlanetRevolveAttributes>("Mars", {150.f, 4000.f, glm::vec3(0.f, 0.f, 1.f)}));
+    attr_map.insert(std::pair<std::string, PlanetRevolveAttributes>("Saturn", {300.f, 5000.f, glm::vec3(0.25f, 0.f, 0.75f)}));
+    attr_map.insert(std::pair<std::string, PlanetRevolveAttributes>("Mercury", {400.f, 6000.f, glm::vec3(0.75f, 0.25f, 0.f)}));
+    attr_map.insert(std::pair<std::string, PlanetRevolveAttributes>("Uranus", {600.f, 7000.f, glm::vec3(0.4f, 0.15f, 0.45f)}));
+    attr_map.insert(std::pair<std::string, PlanetRevolveAttributes>("Venus", {420.f, 8000.f, glm::vec3(0.1f, 0.35f, 0.55f)}));
+    attr_map.insert(std::pair<std::string, PlanetRevolveAttributes>("Venus2", {520.f, 9000.f, glm::vec3(0.55f, 0.25f, 0.f)}));
+    attr_map.insert(std::pair<std::string, PlanetRevolveAttributes>("Neptune", {480.f, 10000.f, glm::vec3(0.3f, 0.45f, 0.25f)}));
 };
 
 void Revolve::set_center(){
@@ -44,6 +50,5 @@ void Revolve::revolve(Scene::Transform *transform, float elapsed){
     planet_vector = glm::normalize(planet_vector) * dist_to_sun;
 
     transform->position = planet_vector;
-    
-    return;    
+    return;
 }
